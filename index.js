686a2515915748ed9d40cde575063cb78996f4aa
Rachel Acosta <denversunrise@gmail.com>
2017-12-18 12:05:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function viewCart() {
     for (var i = 0; i < cart.length - 1; i ++){
       daCart = daCart + `${items[i]} at $${prices[i]}, `;
     }
-    daCart = `${daCart}and ${items[cart.length]} at $${prices[cart.length]}.`;
+    daCart = `${daCart}and ${items[cart.length - 1]} at $${prices[cart.length]}.`;
     console.log(daCart);
   }
   return cart;
